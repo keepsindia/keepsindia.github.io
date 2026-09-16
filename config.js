259a7823958a350d0f7db565ec911809917ab248
keepsindia <keepsindia@gmail.com>
2026-09-16 15:50:47 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,3 @@
-// Paste your deployed Google Apps Script Web App URL between the quotes.
-// Until you do, the site runs in DEMO mode: Dec 6 & Dec 12, 2026 appear booked,
-// and inquiry submissions are not sent anywhere.
 window.KEEPS_CONFIG = {
-  API_URL: "",
-  LAUNCH_DATE: "2026-12-04",
-  DEMO_BOOKED_DATES: ["2026-12-06", "2026-12-12"]
+  API_URL: "https://script.google.com/macros/s/AKfycbxxAj7yiv0BCI7BUxbQU7AdK_2vUAJDM6rzr5slFFUagJIYpubZEcnPVj2EFguUDFEBww/exec"
 };
